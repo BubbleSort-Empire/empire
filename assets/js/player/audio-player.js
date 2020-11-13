b3272aaced85350a29d9a6e2db5305046a74157b
Vic Hudson <vichudson1@gmail.com>
2020-11-13 17:44:08 -0500

    Updates list pages to use new scripts from assets.
    Signed-off-by: Vic Hudson <vichudson1@gmail.com>

--- assets/js/player/audio-player.js
+++ assets/js/player/audio-player.js
@@ -1,3 +1,23 @@
 "use strict";
 
 console.log("Audio Player Script Running.")
+
+$(document).ready(function () {
+  $('audio').mediaelementplayer({
+	defaultSpeed: '1.00',
+	speeds: ['1.25', '1.50', '2.00', '0.75'],
+	loop: true,
+	skipBackInterval: 15,
+	jumpForwardInterval: 15,
+	features: [
+	  "skipback",
+	  "playpause",
+	  "jumpforward",
+	  "speed",
+	  "progress",
+	  "current",
+	  "duration",
+	  "volume",
+	]
+  });
+});
