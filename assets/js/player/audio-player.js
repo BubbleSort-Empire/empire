edfde7e5c1627841080f8d88de918b00cb9cc5ba
Victor Hudson <vichudson1@gmail.com>
2020-11-15 10:03:45 -0500

    Unifies Player includes partials and scripts
    Signed-off-by: Victor Hudson <vichudson1@gmail.com>

--- assets/js/player/audio-player.js
+++ assets/js/player/audio-player.js
@@ -1,23 +1,78 @@
 "use strict";
 
-console.log("Audio Player Script Running.")
+if (document.getElementById("single-player")) {
+	buildSinglePlayer();
+} else {
+	buildListPlayers();
+}
 
-$(document).ready(function () {
-  $('audio').mediaelementplayer({
-	defaultSpeed: '1.00',
-	speeds: ['1.25', '1.50', '2.00', '0.75'],
-	loop: true,
-	skipBackInterval: 15,
-	jumpForwardInterval: 15,
-	features: [
-	  "skipback",
-	  "playpause",
-	  "jumpforward",
-	  "speed",
-	  "progress",
-	  "current",
-	  "duration",
-	  "volume",
-	]
-  });
-});
+function buildListPlayers() {
+	const players = Array.from(document.querySelectorAll("audio"));
+	const options = {
+		defaultSpeed: "1.00",
+		speeds: ["1.25", "1.50", "2.00", "0.75"],
+		loop: true,
+		skipBackInterval: 15,
+		jumpForwardInterval: 15,
+		features: [
+			"skipback",
+			"playpause",
+			"jumpforward",
+			"speed",
+			"progress",
+			"current",
+			"duration",
+			"volume",
+		],
+	};
+
+	players.forEach((p) => {
+		new MediaElementPlayer(p, options);
+	});
+}
+
+function buildSinglePlayer() {
+	const options = {
+		defaultSpeed: "1.00",
+		speeds: ["1.25", "1.50", "2.00", "0.75"],
+		loop: true,
+		skipBackInterval: 15,
+		jumpForwardInterval: 15,
+		features: [
+			"current",
+			"progress",
+			"duration",
+			"speed",
+			"skipback",
+			"playpause",
+			"jumpforward",
+			"volume",
+		],
+	};
+
+	new MediaElementPlayer(document.querySelector("audio"), options);
+
+	// Separate the audio controls so I can style them better.
+	(() => {
+		const elementTop = document.createElement("div");
+		const elementBottom = document.createElement("div");
+		elementTop.classList.add("mejs-prepended-buttons");
+		elementBottom.classList.add("mejs-appended-buttons");
+
+		const controls = document.querySelector(".mejs__controls");
+		controls.prepend(elementTop);
+		controls.append(elementBottom);
+
+		const controlsChildren = Array.from(controls.childNodes).filter((v) =>
+			v.className.startsWith("mejs_")
+		);
+
+		controlsChildren.slice(0, 3).forEach((elem) => {
+			elementTop.append(elem);
+		});
+
+		controlsChildren.slice(3, controlsChildren.length).forEach((elem) => {
+			elementBottom.append(elem);
+		});
+	})();
+}
